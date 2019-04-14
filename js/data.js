b344f07a779db213983439c99382f4b277f70aac
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-04-14 18:53:07 -0400

    firestore funcionando solucionado error de video background

--- js/data.js
+++ js/data.js
@@ -1,7 +1,23 @@
-export const readProject = (project) => {
-  const postReference = firebase.database().ref('projects/');
-  postReference.on('child_added', (post) => {
-      project(post);
+let db = firebase.firestore();
+let project = document.getElementById('projectsId');
+db.collection('projects')
+  .get()
+  .then(function(querySnapshot) {
+    project.innerHTML = '';
+    querySnapshot.forEach(function(doc) {
+      project.innerHTML += `
+     <div class="col-4">
+        <img class="image-proyect" src="${doc.data().image}" alt="" srcset="">
+      <h4 class="title-proyect">${doc.data().name}</BurgerQueen></h4>
+      <p class="description-proyect">${doc.data().description}</p>
+      <div class="row">
+        <div class="col-6"><a href="${
+          doc.data().demo
+        }" target="_blank" class="btn btn-outline-secondary"><i class="fas fa-laptop-code"></i> Demo</a></div>
+        <div class="col-6"><a href="${
+          doc.data().github
+        }" target="_blank" class="btn btn-outline-secondary"><i class="fab fa-github"></i> Github</a></div>
+    </div> 
+   </div>`;
+    });
   });
-  console.log(postReference);
-};
